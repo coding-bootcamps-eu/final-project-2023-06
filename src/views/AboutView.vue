66e9794ef816b6d46578a0a55dacb5b7acb95cc4
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-18 14:15:43 +0200

    feat: adds routing to project

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About us</h1>
+    <p>Credits:</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Our logo here" src="" />
+    <h1>Welcome to Meme Wizard</h1>
+    <p>Create hilarious memes with our easy-to-use tool.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>

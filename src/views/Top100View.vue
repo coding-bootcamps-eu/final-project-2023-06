c7cd9d40912e34b25da4896004cae2a94c105f6a
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-18 16:56:27 +0200

    test: adds top 100 view page

--- src/views/Top100View.vue
+++ src/views/Top100View.vue
@@ -1,5 +1,43 @@
 <template>
-  <h1>Top 100</h1>
+  <div>
+    <h1>Popular Memes</h1>
+    <ul>
+      <li v-for="meme in memes" :key="meme.id">
+        <h3>{{ meme.name }}</h3>
+        <img
+          :src="meme.url"
+          :alt="meme.name"
+          :width="meme.width"
+          :height="meme.height"
+          :box_count="meme.box_count"
+        />
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      memes: [],
+    };
+  },
+  mounted() {
+    this.fetchMemes();
+  },
+  methods: {
+    fetchMemes() {
+      fetch("https://api.imgflip.com/get_memes")
+        .then((response) => response.json())
+        .then((data) => {
+          this.memes = data.data.memes;
+          console.log(this.memes);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>

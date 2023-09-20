8916f129e1e13910a5124a1e6e81eda44a6cbdc2
Melo Lytz <marcelo.lutzenkirchen@gmail.com>
2023-09-20 14:44:48 +0100

    style: textarea

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,21 +4,27 @@
     <h1>Welcome to Meme Wizard</h1>
     <p>Create hilarious memes with our easy-to-use tool.</p>
 
-    <input v-model="imageUrl" placeholder="Paste Image URL here" />
+    <div class="main-area">
+      <input
+        v-model="imageUrl"
+        id="uploadURL"
+        placeholder="Paste Image URL here"
+      />
 
-    <div id="my-node" v-if="imageUrl" class="imageURL">
-      <img :src="imageUrl" alt="Selected Image" />
-      <textarea
-        class="combinedText"
-        v-model="combinedText"
-        placeholder="Enter Text"
-      ></textarea>
-    </div>
-    <button @click="generateMeme">Generate Meme</button>
+      <div id="my-node" v-if="imageUrl" class="imageURL">
+        <img :src="imageUrl" alt="Selected Image" />
+        <textarea
+          class="combinedText"
+          v-model="combinedText"
+          placeholder="Enter Text"
+        ></textarea>
+      </div>
+      <button @click="generateMeme">Generate Meme</button>
 
-    <div v-if="generatedMeme">
-      <img :src="generatedMeme" alt="Generated Meme" />
-      <button @click="downloadMeme">Download Meme</button>
+      <div v-if="generatedMeme">
+        <img :src="generatedMeme" alt="Generated Meme" />
+        <button @click="downloadMeme">Download Meme</button>
+      </div>
     </div>
   </div>
 </template>
@@ -46,11 +52,32 @@ export default {
 };
 </script>
 <style scoped>
+.main-area {
+  display: flex;
+  flex-direction: column;
+}
 .imageURL {
   position: relative;
+  display: inline-block;
+}
+
+#uploadURL {
+  max-width: 100%;
+  height: auto;
+  display: block;
 }
 
 .combinedText {
   position: absolute;
+  color: white;
+  text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black,
+    1px 1px 0 black;
+  bottom: 10px;
+  transform: translateX(-50%);
+  width: 80%;
+  padding: 10px;
+  border: 1px solid #000;
+  background-color: transparent;
+  border-color: transparent;
 }
 </style>

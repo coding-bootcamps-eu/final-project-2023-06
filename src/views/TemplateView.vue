51129e66ae7c29b0eddb449288ec3e9184ef81fe
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-20 13:33:57 +0200

    style: changes the layout to grid

--- src/views/TemplateView.vue
+++ src/views/TemplateView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <h1>Popular Memes</h1>
-    <ul>
-      <li v-for="meme in memes" :key="meme.id">
-        <h3>{{ meme.name }}</h3>
+  <div class="memes-container">
+    <h1>Meme templates you can use</h1>
+    <div class="memes-grid">
+      <div v-for="meme in memes" :key="meme.id" class="memes-item">
+        <h2>{{ meme.name }}</h2>
         <img
           :src="meme.url"
           :alt="meme.name"
           :width="meme.width"
           :height="meme.height"
-          :box_count="meme.box_count"
+          class="meme-image"
         />
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,3 +41,44 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.memes-grid {
+  --gap: 5rem;
+  --num-cols: 3;
+  --row-height: 300px;
+  box-sizing: border-box;
+  padding: var(--gap);
+  display: grid;
+  grid-template-columns: repeat(var(--num-cols), 1fr);
+  grid-auto-rows: var(--row-height);
+  gap: var(--gap);
+}
+
+.memes-grid img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: 0.5s ease-in-out;
+}
+
+.memes-item {
+  margin-bottom: 1rem;
+}
+
+@media screen and (max-width: 1024px) {
+  .memes-grid {
+    --num-cols: 2;
+  }
+}
+
+@media screen and (max-width: 540px) {
+  .memes-grid {
+    --num-cols: 1;
+  }
+
+  h2 {
+    font-size: 0.9rem;
+  }
+}
+</style>

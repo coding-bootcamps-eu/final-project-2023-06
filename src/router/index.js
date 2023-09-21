412852b9904fd4f029330b1e951a084cf69cd2e3
Gabriel Heinrich <gabrielheinrich@pm.me>
2023-09-21 15:46:51 +0200

    Transition to vite

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'home',
+    component: HomeView
   },
   {
-    path: "/about",
-    name: "about",
+    path: '/about',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import("../views/AboutView.vue"),
+    component: () => import('../views/AboutView.vue')
   },
   {
-    path: "/template",
-    name: "template",
-    component: () => import("../views/TemplateView.vue"),
+    path: '/template',
+    name: 'template',
+    component: () => import('../views/TemplateView.vue')
   },
   {
-    path: "/new",
-    name: "new",
-    component: () => import("../views/NewView.vue"),
-  },
-];
+    path: '/new',
+    name: 'new',
+    component: () => import('../views/NewView.vue')
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  history: createWebHistory(),
+  routes
+})
 
-export default router;
+export default router
